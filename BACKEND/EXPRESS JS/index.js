6fb59493e3e32151948a4921777b771434fe059d
Sairaj Patil <sairajpatil035@gmail.com>
2026-03-28 13:35:47 +0530

    day 32

---
+++ BACKEND/EXPRESS JS/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+
+console.dir(app);
+
+let port = 3000;
+
+// app.listen(port, () => {
+//     console.log(`app is listening on port server started  ${port}`);
+// });
+
+// app.use((req,res) => {
+//     console.log(req);
+//         console.log("request recived "); 
+//         let html = "<h1>fruits</h1> <ul>apple</ul> <ul>mango</ul></h1>"  
+//         res.send (html) ;  
+// });
+
+app.listen(port, () => {
+    console.log(`app is listening on port ${port}`);
+
+});
+
+app.get("/",(req, res) => {
+    res.send("you conntacted rooth path ");
+});
+app.get("/apple",(req, res) => {
+    res.send("you conntacted apple     path ");
+});
+app.get("/mango",(req, res) => {
+    res.send("you conntacted mangopath ");
+});
+app.use((req,  res) => {
+    res.status(404).send("this path does not exit");
+});
+ 
