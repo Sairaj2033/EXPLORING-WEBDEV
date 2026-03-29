673d28b5f778a05b09f74a101367391a90c201dd
Sairaj Patil <sairajpatil035@gmail.com>
2026-03-29 15:08:02 +0530

    exploring ejs

--- BACKEND/EXPRESS JS/EJS/index.js
+++ BACKEND/EXPRESS JS/EJS/index.js
@@ -15,8 +15,33 @@ app.get("/hello", (req, res) => {
     res.send("hello");
 });
 
+app.get("/ig/:username", (req, res) => {
+    const followers = ["sairajpati", "om" , "tanmay", "amogh"]
+    let { username } = req.params;
+    res.render("instagram.ejs",{username, followers});
+});
+
+app.get("/instagram/:username", (req, res) => {
+    let {username} = req.params;
+    const instaData = require("./data.json");
+    const data = instaData[username];
+     if(data) {
+        res.render("instagram2.ejs",{data});
+} else {
+    res.render("error.ejs");
+}
+    
+});
+
+
 app.get("/rolldice", (req, res) => {
-    res.send("hello");
+    let diceval= Math.floor(Math.random()*6+1) ; 
+    res.render("rolldice.ejs",{diceval});
+});
+
+app.get("/rolldice6", (req, res) => {
+   let dicev =  Math.floor(Math.random()*6+1) ;  
+   res.render("roll6.ejs",{dicev});
 });
 
 app.listen(port, () => {
